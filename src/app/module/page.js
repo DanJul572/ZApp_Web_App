4002cd862485bfaf1137df0d4741819d05178759
juliandi_dandi <dandi.juliandi@nawadata.id>
2023-09-28 18:52:52 +0700

    make the component table work

--- src/app/module/page.js
+++ src/app/module/page.js
@@ -1,9 +1,46 @@
+'use client';
 import Table from '@/component/table';
+import mock_columns from '@/mock/module/columns';
+import mock_rows from '@/mock/module/rows';
 
 export default function Module() {
+    const onAdd = () => {
+        console.log('add');
+    };
+
+    const onUpdate = val => {
+        console.log(val);
+    };
+
+    const onDelete = val => {
+        console.log(val);
+    };
+
+    const onSearch = val => {
+        console.log(val);
+    };
+
+    const onChangePage = val => {
+        console.log(val);
+    };
+
     return (
         <div>
-            <Table />
+            <Table
+                columnKey={'id'}
+                action={['insert', 'update', 'delete']}
+                rows={mock_rows}
+                columns={mock_columns}
+                pageCount={5}
+                rowCount={25}
+                onAdd={onAdd}
+                onUpdate={onUpdate}
+                onDelete={onDelete}
+                onSearch={onSearch}
+                onChangePage={onChangePage}
+                enableSearch={true}
+                enablePagination={true}
+            />
         </div>
     );
 }
--- src/component/table/index.js
+++ src/component/table/index.js
@@ -1,53 +1,136 @@
 'use client';
-import React, {useMemo} from 'react';
+import React, {useEffect, useState} from 'react';
 import {MaterialReactTable} from 'material-react-table';
+import {Box, Button, IconButton} from '@mui/material';
+import {Delete, Edit} from '@mui/icons-material';
 
-const data = [
-    {
-        name: {
-            firstName: 'John',
-            lastName: 'Doe',
-        },
-        address: '261 Erdman Ford',
-        city: 'East Daphne',
-        state: 'Kentucky',
-    },
-];
+const Table = props => {
+    const {
+        action, // insert, update, delete
+        columnKey,
+        columns = [],
+        enableDensityToggle = false,
+        enableFilter = false,
+        enableFullScreenToggle = false,
+        enableHiding = false,
+        enablePagination = false,
+        enableRowSelection = false,
+        enableSearch = false,
+        enableSorting = false,
+        onAdd,
+        onChangePage,
+        onDelete,
+        onFilter,
+        onSelect,
+        onSearch,
+        onSort,
+        onUpdate,
+        pageCount = 0,
+        rowCount = 0,
+        rows = [],
+    } = props;
 
-const Table = () => {
-    //should be memoized or stable
-    const columns = useMemo(
-        () => [
-            {
-                accessorKey: 'name.firstName', //access nested data with dot notation
-                header: 'First Name',
-                size: 150,
+    const [pagination, setPagination] = useState({
+        pageIndex: 1,
+        pageSize: pageCount,
+    });
+    const [columnFilters, setColumnFilters] = useState([]);
+    const [sorting, setSorting] = useState([]);
+    const [rowSelection, setRowSelection] = useState({});
+
+    const actionColumn = () => {
+        if (!action.length && !action.filter(item => item !== 'insert').length)
+            return false;
+        return {
+            'mrt-row-actions': {
+                muiTableHeadCellProps: {
+                    align: 'center',
+                },
+                size: 120,
             },
-            {
-                accessorKey: 'name.lastName',
-                header: 'Last Name',
-                size: 150,
-            },
-            {
-                accessorKey: 'address', //normal accessorKey
-                header: 'Address',
-                size: 200,
-            },
-            {
-                accessorKey: 'city',
-                header: 'City',
-                size: 150,
-            },
-            {
-                accessorKey: 'state',
-                header: 'State',
-                size: 150,
-            },
-        ],
-        [],
+        };
+    };
+
+    const insertButton = () => {
+        if (!action.length && !action.find(item => item === 'insert')) return;
+        return (
+            <Button color="primary" onClick={onAdd} variant="contained">
+                Create New Data
+            </Button>
+        );
+    };
+
+    const isSupportAction = () => {
+        return action.filter(item => item !== 'insert').length ? true : false;
+    };
+
+    const actionButton = row => {
+        return (
+            <Box sx={{display: 'flex', alignItems: 'center'}}>
+                {action.length && action.find(item => item === 'update') && (
+                    <IconButton onClick={() => onUpdate(row.original)}>
+                        <Edit />
+                    </IconButton>
+                )}
+                {action.length && action.find(item => item === 'delete') && (
+                    <IconButton onClick={() => onDelete(row.original)}>
+                        <Delete />
+                    </IconButton>
+                )}
+            </Box>
+        );
+    };
+
+    useEffect(() => {
+        if (enablePagination) onChangePage(pagination.pageIndex);
+    }, [pagination]);
+
+    useEffect(() => {
+        if (enableFilter) onFilter(columnFilters);
+    }, [columnFilters]);
+
+    useEffect(() => {
+        if (enableSorting) onSort(sorting);
+    }, [sorting]);
+
+    useEffect(() => {
+        if (enableRowSelection) onSelect(Object.keys(rowSelection));
+    }, [rowSelection]);
+
+    return (
+        <MaterialReactTable
+            columns={columns}
+            data={rows}
+            displayColumnDefOptions={actionColumn}
+            enableColumnActions={false}
+            enableColumnFilters={enableFilter}
+            enableDensityToggle={enableDensityToggle}
+            enableEditing={isSupportAction}
+            enableFilterMatchHighlighting={false}
+            enableFullScreenToggle={enableFullScreenToggle}
+            enableGlobalFilter={enableSearch}
+            enableHiding={enableHiding}
+            enablePagination={enablePagination}
+            enableRowSelection={enableRowSelection}
+            enableSorting={enableSorting}
+            getRowId={row => row[columnKey]}
+            manualFiltering
+            manualPagination
+            manualSorting
+            muiTablePaginationProps={{rowsPerPageOptions: [10]}}
+            onColumnFiltersChange={setColumnFilters}
+            onGlobalFilterChange={onSearch}
+            onPaginationChange={setPagination}
+            onRowSelectionChange={setRowSelection}
+            onSortingChange={setSorting}
+            pageCount={pageCount}
+            positionActionsColumn="last"
+            renderRowActions={({row}) => actionButton(row)}
+            renderTopToolbarCustomActions={insertButton}
+            rowCount={rowCount}
+            state={{pagination, columnFilters, sorting, rowSelection}}
+        />
     );
-
-    return <MaterialReactTable columns={columns} data={data} />;
 };
 
 export default Table;
