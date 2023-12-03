1e8a9f79f51884a4adccc37a1b20d54f8cb2432d
juliandi_dandi <dandi.juliandi@nawadata.id>
2023-12-03 19:15:44 +0700

    add delete module process

--- src/app/module/page.js
+++ src/app/module/page.js
@@ -3,7 +3,9 @@
 import {useEffect, useState} from 'react';
 import {useRouter} from 'next/navigation';
 import {useAlert} from '@/context/AlertProvider';
+import {useLoading} from '@/context/LoadingProvider';
 
+import Confirm from '@/component/dialog/Confirm';
 import Table from '@/component/table';
 
 import CActionType from '@/constant/CActionType';
@@ -13,13 +15,15 @@ import request from '@/helper/request';
 export default function Module() {
     const {push} = useRouter();
     const {setAlert} = useAlert();
+    const {setLoading} = useLoading();
 
-    const [loading, setLoading] = useState(true);
     const [page, setPage] = useState(1);
     const [filter, setFilter] = useState([]);
     const [sort, setSort] = useState([]);
     const [rows, setRows] = useState([]);
     const [rowCount, setRowCount] = useState(10);
+    const [rowSelected, setRowSelected] = useState(null);
+    const [openConfirmDialog, setOpenConfirmDialog] = useState(false);
 
     const actionList = [
         {
@@ -34,6 +38,47 @@ export default function Module() {
 
     const onCLickToolbarAction = action => {
         if (action.value === CActionType.insert.value) push('/module/create');
+    };
+
+    const onClickRowAction = data => {
+        if (data.action.value === CActionType.delete.value) {
+            setOpenConfirmDialog(true);
+            setRowSelected(data.row);
+        }
+    };
+
+    const onConfirm = confirm => {
+        if (confirm) onDelete();
+        setOpenConfirmDialog(false);
+    };
+
+    const onDelete = () => {
+        setLoading(true);
+
+        const body = {
+            id: rowSelected.id,
+        };
+
+        request
+            .post('/module/delete', body)
+            .then(res => {
+                getRows();
+                setAlert({
+                    status: true,
+                    type: 'success',
+                    message: res,
+                });
+            })
+            .catch(err => {
+                setAlert({
+                    status: true,
+                    type: 'error',
+                    message: err,
+                });
+            })
+            .finally(() => {
+                setLoading(false);
+            });
     };
 
     const getRows = () => {
@@ -82,15 +127,22 @@ export default function Module() {
                 enablePagination={true}
                 enableFilter={true}
                 enableSorting={true}
-                isLoading={loading}
                 onChangePage={setPage}
-                onClickRowAction={() => {}}
+                onClickRowAction={onClickRowAction}
                 onClickToolbarAction={onCLickToolbarAction}
                 onFilter={setFilter}
                 onSort={setSort}
                 pageIndex={0}
                 rowCount={rowCount}
                 rows={rows}
+            />
+            <Confirm
+                open={openConfirmDialog}
+                title="Delete Module"
+                text="Are you sure you want to delete this module ?"
+                confirmButton="Delete"
+                cancelButton="Cancel"
+                onConfirm={onConfirm}
             />
         </div>
     );
