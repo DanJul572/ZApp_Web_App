f8ad80b465f14843e8a21623e28f56201523c47c
Dandi <dandi.juliandi@nawadata.id>
2024-03-16 14:34:37 +0700

    validation setOpen in drawer component

--- src/component/container/Drawer.js
+++ src/component/container/Drawer.js
@@ -5,8 +5,14 @@ import MuiDrawer from '@/alias/MuiDrawer';
 const Drawer = props => {
     const {open, children, setOpen} = props;
 
+    const onOpen = () => {
+        if (setOpen) {
+            setOpen(false);
+        }
+    };
+
     return (
-        <MuiDrawer open={open} anchor="right" onClose={() => setOpen(false)}>
+        <MuiDrawer open={open} anchor="right" onClose={onOpen}>
             <Box padding={2} width={750}>
                 {children}
             </Box>
