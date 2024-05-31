e57d8999ce9f610011c10ea2805e42a5e1a2d3fe
Dandi <dandi.juliandi@nawadata.id>
2024-05-31 08:13:44 +0700

    enable webpack cache

--- next.config.js
+++ next.config.js
@@ -1,15 +1,3 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    webpack: (config, {buildId, dev, isServer, defaultLoaders, nextRuntime, webpack}) => {
-        if (config.cache && !dev) {
-            config.cache = Object.freeze({
-                type: 'memory',
-            });
-            config.cache.maxMemoryGenerations = 0;
-        }
-        // Important: return the modified config
-        return config;
-    },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
