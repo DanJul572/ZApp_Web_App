8ec44cc210d12cbe6e22246dcec3996f27f47c9e
Dandi <dandi.juliandi@nawadata.id>
2024-05-31 19:59:25 +0700

    add tooltip in short text component

--- src/component/input/ShortText.js
+++ src/component/input/ShortText.js
@@ -2,7 +2,10 @@ import {useContext, useEffect} from 'react';
 
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
+
+import Help from '@mui/icons-material/Help';
 
 import {ErrorContext} from '@/context/ErrorProvider';
 import {validator} from '@/helper/validator';
@@ -10,7 +13,7 @@ import {validator} from '@/helper/validator';
 import CTheme from '@/constant/CTheme';
 
 const ShortText = props => {
-    const {label, onChange, value, rules, group, name, disabled, onBlur, placeholder} = props;
+    const {label, onChange, value, rules, group, name, disabled, onBlur, placeholder, tooltip} = props;
 
     const {setError, clearError} = useContext(ErrorContext);
 
@@ -36,7 +39,14 @@ const ShortText = props => {
 
     return (
         <Box>
-            <Typography fontSize={CTheme.font.size.value}>{label}</Typography>
+            <Box display="flex" justifyContent="space-between" alignItems="center">
+                <Typography fontSize={CTheme.font.size.value}>{label}</Typography>
+                {tooltip && (
+                    <Tooltip title={tooltip} arrow placement="top">
+                        <Help sx={{fontSize: CTheme.font.size.value}} />
+                    </Tooltip>
+                )}
+            </Box>
             <TextField
                 disabled={disabled}
                 variant="outlined"
