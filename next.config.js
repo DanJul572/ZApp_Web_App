b46f9bba38a10f858b4e556c3fb70c1583bf2915
Dandi <dandi.juliandi@nawadata.id>
2024-06-05 19:11:13 +0700

    disable caching in production mode

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, {dev}) => {
+        if (config.cache && !dev) {
+            config.cache = Object.freeze({
+                type: 'memory',
+            });
+            config.cache.maxMemoryGenerations = 0;
+        }
+        // Important: return the modified config
+        return config;
+    },
+};
 
 module.exports = nextConfig;
